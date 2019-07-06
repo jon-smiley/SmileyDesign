e0efb5545e042694ef564da192252af535391395
Jon Smiley <jasmiley0715@gmail.com>
2019-07-06 15:31:01 -0500

    SPD-1: Add logic that will indicate that the nav bar should be on the right side

--- smiley-design-src/src/components/NavBar.js
+++ smiley-design-src/src/components/NavBar.js
@@ -48,17 +48,22 @@ class NavBar extends Component {
         for (var classString in renderElementArray){
             classStyleString += this.findCssModules(renderElementArray[classString]) + globalValues.space;
         }
-        
+        var navFloatRight = (navSidebarPosition === globalValues.right ? styleSheet.navFloatRight : globalValues.space);
+
        if (navSidebarPosition){
-           classStyleString += styleSheet.navSideDisplayEdit;
+            if(navSidebarPosition === globalValues.right){
+                classStyleString += styleSheet.navSideDisplayRightEdit;
+            } else {
+                classStyleString += styleSheet.navSideDisplayLeftEdit;
+            }
        } else {
-        classStyleString += styleSheet.navTopPositioningBoxShadow;
+            classStyleString += styleSheet.navTopPositioningBoxShadow;
        }
 
         return (
             <Fragment>
                 {/* changing nav bar, top bar */}
-                <div>
+                <div className={navFloatRight}>
                     <div className={navTopPositioning}>
                         <div className={styleSheet.navBarDefault + classStyleString}>
                                 {children}
